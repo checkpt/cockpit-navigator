3103f4a87304d6c8454159fb365500e5d2fd19b6
ajithkumarnallas <ajithkumarv@nallas.com>
2026-04-13 16:39:37 +0530

    Added locale load in different file

--- navigator/load-locale.js
+++ navigator/load-locale.js
@@ -1,7 +1,11 @@
-var lang = cockpit.language;
-console.log("Language: " + lang);
-console.log(lang, lang !== "en", /^[a-zA-Z_]+$/.test(lang));
+var lang = null;
+var match = document.cookie.match(/(?:^|;\s*)CockpitLang=([a-zA-Z_]+)/);
+if (match) {
+    lang = match[1];
+}
+if (!lang && navigator.language) {
+    lang = navigator.language.replace("-", "_").split("_")[0];
+}
 if (lang && lang !== "en" && /^[a-zA-Z_]+$/.test(lang)) {
     document.write('\x3Cscript src="po.' + lang + '.js">\x3C/script>');
-    console.log("Loaded locale: " + lang);
 }
